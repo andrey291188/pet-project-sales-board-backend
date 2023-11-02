385eb492e9fafdac357c7f33412a718242136097
Andrey <andrey291188@gmail.com>
2023-11-02 21:29:24 +0200

    up

--- controllers/ads/getAllAds.js
+++ controllers/ads/getAllAds.js
@@ -1,26 +1,46 @@
 const { Ads } = require("../../models");
 
 const getAllAds = async (rq, rs) => {
-  const favoriteQuery = rq.query.favorite;
 
-  const { page = 1, limit = 10 } = rq.query;
+  const request =
+    rq.query.q === undefined || rq.query.q === ""
+      ? null
+      : rq.query.q.toLowerCase();
+
+  const regex = new RegExp(request, "i");
+
+  const { page = 1, limit = 12 } = rq.query;
   const skip = (page - 1) * limit;
 
-  const adsList =
-    favoriteQuery !== undefined
-      ? await Ads.find({ favorite: favoriteQuery, active: true }, "-createdAt -updateAt", {
-          skip,
-          limit,
-        })
-      : await Ads.find({active: true}, "-createdAt -updateAt");
+  let adsList = null;
+  let totalAdsCount = null
 
- 
+  if (request !== null) {
+    totalAdsCount = await Ads.countDocuments({ active: true, title: { $regex: regex } })
+    adsList = await Ads.find(
+      { active: true, title: { $regex: regex } },
+      "-createdAt -updateAt",
+      {
+        skip,
+        limit,
+      }
+    );
+  } else {
+    totalAdsCount = await Ads.countDocuments({ active: true })
+    adsList = await Ads.find({ active: true }, "-createdAt -updateAt",
+    {
+      skip,
+      limit,
+    });
+  }
+
 
   rs.json({
     status: "Success",
     code: 200,
     data: {
       result: adsList,
+      totalAds: totalAdsCount,
     },
   });
 };
